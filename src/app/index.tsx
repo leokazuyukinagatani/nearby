554597e1b6dc0468493d3e903b44e9cca11639b0
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-12-09 22:17:46 -0300

    feat: welcome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
+import { Welcome } from "@/components/welcome";
 import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 24,
+        gap: 24
+      }}
+    >
+      <Welcome/>
     </View>
   )
 }
---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,17 @@
+import { Image, Text, View } from "react-native";
+
+import { colors } from "@/styles/theme";
+import { s } from "./styles";
+
+export function Welcome() {
+  return (
+    <View>
+      <Image
+        source={require("@/assets/logo.png")}
+        style={s.logo}
+      />
+      <Text style={s.title}>Boas vindas ao Nearby!</Text>
+      <Text style={s.subtitle}>Tenha cupons de vantagens para usar {"\n"} em seus estabelecimentos favoritos.</Text>
+    </View>
+  )
+}
