8710ccbc60c21ca4b5eef86aaf5968c0fba5147b
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-12-09 22:07:13 -0300

    config: fonts and loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,13 @@
+import { ActivityIndicator } from "react-native";
+
+import { colors } from "@/styles/theme";
+import { s } from "./styles";
+
+export function Loading() {
+  return (
+      <ActivityIndicator
+          color={colors.green.base}
+          style={s.container}
+      />      
+  )
+}
