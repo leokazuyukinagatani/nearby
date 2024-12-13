7d4e2f92924e4a48c29cb58fe6f09599e28188fa
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-12-13 04:50:55 -0300

    feat: details

---
+++ src/components/market/cover/index.tsx
@@ -0,0 +1,25 @@
+import { ImageBackground, View } from "react-native";
+
+import { s } from "./styles";
+import { IconArrowLeft } from "@tabler/icons-react-native";
+import { Button } from "@/components/button";
+import { router } from "expo-router";
+
+type Props = {
+    uri: string;
+};
+
+export function Cover({ uri }: Props) {
+    return (
+        <ImageBackground source={{ uri }} style={s.container}>
+            <View style={s.header}>
+                <Button
+                    style={{ width: 40, height: 40 }}
+                    onPress={() => router.back()}
+                >
+                    <Button.Icon icon={IconArrowLeft} />
+                </Button>
+            </View>
+        </ImageBackground>
+    );
+}
--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -1,5 +1,4 @@
 import {
-    ActivityIndicator,
     Image,
     Text,
     TouchableOpacity,
@@ -30,11 +29,15 @@ export function Place({ data, ...rest }: Props) {
             <Image style={s.image} source={{ uri: data.cover }} />
             <View style={s.content}>
                 <Text style={s.name}>{data.name}</Text>
-                <Text style={s.description} numberOfLines={2}>{data.description}</Text>
+                <Text style={s.description} numberOfLines={2}>
+                    {data.description}
+                </Text>
 
                 <View style={s.footer}>
                     <IconTicket size={16} color={colors.red.base} />
-                    <Text style={s.tickets}>{`${data.coupons} cupons disponíveis`}</Text>
+                    <Text
+                        style={s.tickets}
+                    >{`${data.coupons} cupons disponíveis`}</Text>
                 </View>
             </View>
         </TouchableOpacity>
